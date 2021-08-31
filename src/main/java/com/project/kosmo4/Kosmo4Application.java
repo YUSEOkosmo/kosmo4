c072bb257cd41ead3919f1dc77225cbf855c55a5
YUSEOkosmo <88265178+YUSEOkosmo@users.noreply.github.com>
2021-08-31 17:35:54 +0900

    first commit

---
+++ src/main/java/com/project/kosmo4/Kosmo4Application.java
@@ -0,0 +1,13 @@
+package com.project.kosmo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosmo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosmo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kosmo4/Kosmo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kosmo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kosmo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
